164e325d0b5a6ab23a7965e9f4d7a20242bd86ea
LucianCrainic <crainic.lucian@gmail.com>
2024-11-09 16:57:57 +0100

    [UPDATE] iris ce layout

--- keyboards/keebio/iris_ce/keymaps/luciancrainic/config.h
+++ keyboards/keebio/iris_ce/keymaps/luciancrainic/config.h
@@ -1,19 +1,1 @@
-// Configure the global tapping term (default: 200ms)
 #pragma once
-
-#define TAPPING_TERM 200
-
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 0
-
-// Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LSFT_T(KC_D)
-#define HOME_F LCTL_T(KC_F)
-
-// Right-hand home row mods
-#define HOME_J RCTL_T(KC_J)
-#define HOME_K RSFT_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_SCLN RGUI_T(KC_SCLN)
--- keyboards/keebio/iris_ce/keymaps/luciancrainic/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/luciancrainic/keymap.c
@@ -1,28 +1,94 @@
+/**
+ * @file keymap.c
+ * @brief Custom keymap for the Keebio Iris CE keyboard.
+ *
+ * This file contains the implementation of custom keycodes and their corresponding
+ * string outputs for the Keebio Iris CE keyboard.
+ */
 #include QMK_KEYBOARD_H
 
-// enum that defines the custom keycodes implemented in the keymap.
 enum custom_keycodes {
     PASSWD = SAFE_RANGE,
+    //à è ì ò ù é ó
+    // ITA_E=SAFE_RANGE+1,
+    // ITA_E2=SAFE_RANGE+2,
+    // ITA_I=SAFE_RANGE+3,
+    // ITA_O=SAFE_RANGE+4,
+    // ITA_O2=SAFE_RANGE+5,
+    // ITA_U=SAFE_RANGE+6,
+    // ITA_A=SAFE_RANGE+7,
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        // this case handles all the custom keycodes.
-        case PASSWD:
-            if (record->event.pressed) {
-                SEND_STRING("22!ShelbyCat11");
-            }
-            return false;
-    }
-    return true;
-}
 
 enum custom_layers {
      _DELTA,
      _BETA,
      _GAMMA,
 };
+/**
+ * @brief Sends a custom string based on the provided keycode.
+ *
+ * This function maps specific keycodes to their corresponding string outputs.
+ * It is used to send predefined strings when certain keys are pressed.
+ *
+ * @param keycode The custom keycode that triggers the string output.
+ */
+void send_custom_string(uint16_t keycode) {
+    switch (keycode) {
+        case PASSWD:
+            SEND_STRING("22!ShelbyCat11");
+            break;
+        // case ITA_E:
+        //     SEND_STRING("è");
+        //     break;
+        // case ITA_E2:
+        //     SEND_STRING("é");
+        //     break;
+        // case ITA_I:
+        //     SEND_STRING("ì");
+        //     break;
+        // case ITA_O:
+        //     SEND_STRING("ò");
+        //     break;
+        // case ITA_O2:
+        //     SEND_STRING("ó");
+        //     break;
+        // case ITA_U:
+        //     SEND_STRING("ù");
+        //     break;
+        // case ITA_A:
+        //     SEND_STRING("à");
+        //     break;
+    }
+}
 
+/**
+ * Processes a keycode when a key event occurs.
+ *
+ * @param keycode The keycode of the key event.
+ * @param record A pointer to the keyrecord_t structure containing information about the key event.
+ * @return Returns false if the key event was handled and no further processing is needed, true otherwise.
+ */
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        send_custom_string(keycode);
+        return false;
+    }
+    return true;
+}
+
+/**
+ * This file defines the keymap layers for the Iris keyboard.
+ *
+ * Layers:
+ * - _DELTA: Default layer with standard QWERTY layout and some custom key bindings.
+ * - _BETA: Secondary layer with custom key bindings for special characters and shortcuts.
+ * - _GAMMA: Tertiary layer with function keys and mouse controls.
+ *
+ * Keymap Layout:
+ * - Each layer is defined using the LAYOUT macro.
+ * - The keymap is a 2D array where each element represents a key in the matrix.
+ * - Special keycodes and macros are used for custom functionality.
+ */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DELTA] = LAYOUT(
@@ -31,27 +97,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_MINS,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_SLSH,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_GRV,   HOME_A,  HOME_S,  HOME_D,  HOME_F,  KC_G,                                KC_H,   HOME_J,  HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
+     KC_GRV,  LGUI_T(KC_A), LALT_T(KC_S),  LSFT_T(KC_D),  LCTL_T(KC_F),  KC_G,        KC_H, RCTL_T(KC_J), RSFT_T(KC_K),   LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_HYPR, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TAB,            CW_TOGG,  KC_N,    KC_M,    KC_COMM, KC_DOT , KC_EQL, KC_MEH,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                     TT(1),   KC_ENT,  KC_SPC,                   KC_BSPC, KC_ESC,  TT(2)
+                                    MO(1),   KC_ENT,  KC_SPC,                   KC_BSPC, KC_ESC,  MO(2)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_BETA] = LAYOUT(
-  //┌────────┬────────┬────────┬───────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______,  _______, C(KC_Z), C(KC_A), RCS(KC_V), RCS(KC_C),                       C(KC_PGUP), KC_PGDN, KC_PGUP, C(KC_PGDN), _______, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_QUOTE, KC_LABK, KC_RABK, KC_DQUO, KC_DOT,                            KC_AMPR, KC_SCLN, KC_LBRC, KC_RBRC, KC_PERC, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_HASH,                            KC_PIPE, KC_COLN, KC_LPRN, KC_RPRN, KC_QUES, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_CIRC,KC_SLSH, KC_ASTR, KC_BSLS, KC_UNDS, _______,          _______, KC_TILD,  KC_DLR, KC_LCBR, KC_RCBR, KC_AT, _______,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, _______
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-  ),
+//   [_BETA] = LAYOUT(
+//   //┌────────┬────────┬────────┬───────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+//      ITA_A,  _______, C(KC_Z), C(KC_A), RCS(KC_V), RCS(KC_C),                       C(KC_PGUP), KC_PGDN, KC_PGUP, C(KC_PGDN), _______, ITA_O,
+//   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+//      ITA_E, KC_QUOTE, KC_LABK, KC_RABK, KC_DQUO, KC_DOT,                            KC_AMPR, KC_SCLN, KC_LBRC, KC_RBRC, KC_PERC, ITA_O2,
+//   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+//      ITA_E2, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_HASH,                            KC_PIPE, KC_COLN, KC_LPRN, KC_RPRN, KC_QUES, ITA_U,
+//   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+//      ITA_I, KC_CIRC,KC_SLSH, KC_ASTR, KC_BSLS, KC_UNDS, _______,          _______, KC_TILD,  KC_DLR, KC_LCBR, KC_RCBR, KC_AT, _______,
+//   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+//                                     _______, _______, _______,                   _______, _______, _______
+//                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+//   ),
 
   [_GAMMA] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
@@ -69,7 +135,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-// The following code is used to disable the RGB lighting when the CAPS_LOCK key is pressed.
+/**
+ * @brief Toggles the RGB lighting based on the Caps Word state.
+ *
+ * This function is called when the Caps Word feature is activated or deactivated.
+ * When activated, it disables the RGB lighting. When deactivated, it enables the RGB lighting.
+ *
+ * @param active A boolean indicating whether the Caps Word feature is active.
+ */
 void caps_word_set_user(bool active) {
     if (active) {
         rgblight_disable();
